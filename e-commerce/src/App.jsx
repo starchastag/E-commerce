20ef08c4a17271ac81f896e9fa87e834dbe459fe
shubham kumar <90451740+starchastag@users.noreply.github.com>
2024-01-03 02:38:16 +0530

    working on Product file almost donw

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,12 @@
+import Product from "./Pages/Product";
+import Home from "./Pages/Home";
+import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Cart from "./Pages/Cart";
+
+const App = () => {
+  return <Home/>;
+};
+
+export default App;
